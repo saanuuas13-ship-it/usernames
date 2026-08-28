ecbb07b06e3d0fbff079b6bfecb1eb8f3df049e7
SanikaAS <saanuuas13@gmail.com>
2026-08-28 11:27:34 +0530

    modified the changes

--- names.py
+++ names.py
@@ -1,4 +1,6 @@
 name=input("Enter the name")
 surname=input("Enter your surname")
+contact=input("Enter your contact details")
 
 print("your details:",name+" "+surname)
+print("your contact is:" , contact)
